1a07a346062c670e4c138a14283a19982bfb9de6
Joanna 'Rezi' S <58380975+Reziya@users.noreply.github.com>
2021-11-21 19:28:47 -0800

    Initial commit, sample code

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace HW4_MandelbrotZoom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIterations = new System.Windows.Forms.Label();
+            this.lblHowToZoom = new System.Windows.Forms.Label();
+            this.tboxMaxIter = new System.Windows.Forms.TextBox();
+            this.bttnDraw = new System.Windows.Forms.Button();
+            this.bttnReset = new System.Windows.Forms.Button();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblIterations
+            // 
+            this.lblIterations.AutoSize = true;
+            this.lblIterations.Location = new System.Drawing.Point(11, 16);
+            this.lblIterations.Name = "lblIterations";
+            this.lblIterations.Size = new System.Drawing.Size(53, 13);
+            this.lblIterations.TabIndex = 0;
+            this.lblIterations.Text = "Iterations:";
+            // 
+            // lblHowToZoom
+            // 
+            this.lblHowToZoom.Location = new System.Drawing.Point(11, 185);
+            this.lblHowToZoom.Name = "lblHowToZoom";
+            this.lblHowToZoom.Size = new System.Drawing.Size(150, 55);
+            this.lblHowToZoom.TabIndex = 1;
+            this.lblHowToZoom.Text = "Click and drag on the image to draw a box, and the program will zoom in on that b" +
+    "ox.";
+            this.lblHowToZoom.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tboxMaxIter
+            // 
+            this.tboxMaxIter.Location = new System.Drawing.Point(71, 13);
+            this.tboxMaxIter.Name = "tboxMaxIter";
+            this.tboxMaxIter.Size = new System.Drawing.Size(80, 20);
+            this.tboxMaxIter.TabIndex = 2;
+            this.tboxMaxIter.Text = "1024";
+            // 
+            // bttnDraw
+            // 
+            this.bttnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bttnDraw.Location = new System.Drawing.Point(24, 65);
+            this.bttnDraw.Name = "bttnDraw";
+            this.bttnDraw.Size = new System.Drawing.Size(120, 35);
+            this.bttnDraw.TabIndex = 3;
+            this.bttnDraw.Text = "Draw";
+            this.bttnDraw.UseVisualStyleBackColor = true;
+            this.bttnDraw.Click += new System.EventHandler(this.BttnDraw_Click);
+            // 
+            // bttnReset
+            // 
+            this.bttnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bttnReset.Location = new System.Drawing.Point(24, 120);
+            this.bttnReset.Name = "bttnReset";
+            this.bttnReset.Size = new System.Drawing.Size(120, 35);
+            this.bttnReset.TabIndex = 4;
+            this.bttnReset.Text = "Reset";
+            this.bttnReset.UseVisualStyleBackColor = true;
+            this.bttnReset.Click += new System.EventHandler(this.BttnReset_Click);
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.picCanvas.Location = new System.Drawing.Point(169, 13);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(600, 600);
+            this.picCanvas.TabIndex = 5;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.PicCanvas_Paint);
+            this.picCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PicCanvas_MouseDown);
+            this.picCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PicCanvas_MouseMove);
+            this.picCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PicCanvas_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 626);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.bttnReset);
+            this.Controls.Add(this.bttnDraw);
+            this.Controls.Add(this.tboxMaxIter);
+            this.Controls.Add(this.lblHowToZoom);
+            this.Controls.Add(this.lblIterations);
+            this.Name = "Form1";
+            this.Text = "Mandelbrot Set";
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIterations;
+        private System.Windows.Forms.Label lblHowToZoom;
+        private System.Windows.Forms.TextBox tboxMaxIter;
+        private System.Windows.Forms.Button bttnDraw;
+        private System.Windows.Forms.Button bttnReset;
+        private System.Windows.Forms.PictureBox picCanvas;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW4_MandelbrotZoom
+{
+    public partial class Form1 : Form
+    {
+        // Variables for the graph
+        const double BAILOUT = 200;
+
+        double xMin, xMax, yMin, yMax;
+        int maxIters;
+
+        // Variables for the zoom function
+        Point startPos, currentPos, tempPos;
+        Rectangle zoomBox;
+        bool isDrawing;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Draws initial Mandelbrot set based on input (max number of iterations)
+        // Default graph is set to [-2, 2] on both axes
+        private void BttnDraw_Click(object sender, EventArgs e)
+        {
+            xMin = -2;
+            xMax = 2;
+            yMin = -2;
+            yMax = 2;
+
+            DrawMandelbrot(xMin, xMax, yMin, yMax);
+        }
+
+        private void DrawMandelbrot(double realMin, double realMax, double imagMin, double imagMax)
+        {
+            int result = 0;
+            bool success = Int32.TryParse(tboxMaxIter.Text, out result);
+
+            // Check if input is valid
+            if (success)
+            {
+                maxIters = result;
+
+                if (maxIters <= 0)
+                {
+                    MessageBox.Show("Please enter an integer greater than 0.");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid integer.");
+                return;
+            }
+
+            // Create bitmap
+            Bitmap img = new Bitmap(picCanvas.ClientSize.Width, picCanvas.ClientSize.Height);
+            Graphics gfx = Graphics.FromImage(img);
+            gfx.Clear(picCanvas.BackColor);
+            picCanvas.Image = img;
+
+            // Calculate values for fractal (using an unoptimized escape time algorithm)
+            double zx = 0.0, zy = 0.0;
+            double cx = 0.0, cy = 0.0;
+            double compSquared = 0.0;
+            double scaleX = Math.Abs(realMax - realMin) / picCanvas.ClientSize.Width;
+            double scaleY = Math.Abs(imagMax - imagMin) / picCanvas.ClientSize.Height;
+
+            for (int px = 0; px < picCanvas.ClientSize.Width; ++px)
+            {
+                cx = realMin + (px * scaleX);
+
+                for (int py = 0; py < picCanvas.ClientSize.Height; ++py)
+                {
+                    cy = imagMin + (py * scaleY);
+                    zx = 0.0;
+                    zy = 0.0;
+                    compSquared = 0.0;
+
+                    int iterations = 0;
+
+                    while (compSquared <= BAILOUT && iterations < maxIters)
+                    {
+                        double tempX = (zx * zx) - (zy * zy) + cx;
+                        zy = (2 * zx * zy) + cy;
+                        zx = tempX;
+
+                        compSquared = (zx * zx) + (zy * zy);
+                        ++iterations;
+                    }
+
+                    // If that pixel is part of the set, then it is colored black
+                    // Otherwise, it's colored royal blue
+                    if (iterations < maxIters)
+                        img.SetPixel(px, py, Color.RoyalBlue);
+                    else
+                        img.SetPixel(px, py, Color.Black);
+                }
+            }
+
+            picCanvas.Image = img;
+        }
+
+        // Set starting point of zoomBox on mouse-click
+        private void PicCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPos = e.Location;
+
+            if (picCanvas.Image != null)
+                isDrawing = true;
+        }
+
+        // "Locks" startPos as a corner while drawing zoomBox
+        // Forces zoomBox to be a square since picCanvas is also a square
+        private void PicCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            Size squareLen;
+
+            currentPos = e.Location;
+
+            if (isDrawing)
+            {
+                int length = Math.Max(
+                    Math.Abs(startPos.X - currentPos.X),
+                    Math.Abs(startPos.Y - currentPos.Y));
+
+                // Negative X Quadrants (with respect to startPos)
+                if (currentPos.X < startPos.X)
+                {
+                    tempPos.X = startPos.X - length;
+
+                    if (currentPos.Y < startPos.Y)
+                        tempPos.Y = startPos.Y - length;
+                    else
+                        tempPos.Y = startPos.Y;
+                }
+                // Positive X Quadrants
+                else
+                {
+                    tempPos.X = startPos.X;
+
+                    if (currentPos.Y < startPos.Y)
+                        tempPos.Y = startPos.Y - length;
+                    else
+                        tempPos.Y = startPos.Y;
+                }
+
+                squareLen = new Size(length, length);
+                zoomBox = new Rectangle(tempPos, squareLen);
+
+                picCanvas.Invalidate();
+            }
+        }
+
+        // Draws zoomBox while the mouse button is held down
+        private void PicCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (isDrawing)
+                e.Graphics.DrawRectangle(Pens.White, zoomBox);
+        }
+
+        // Get final end point of zoomBox once mouse button is released
+        // Then draw zoomed in Mandelbrot set
+        private void PicCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                isDrawing = false;
+                picCanvas.Invalidate();
+
+                double newXMin = xMin + (zoomBox.Left * ((xMax - xMin) / picCanvas.ClientSize.Width));
+                double newYMin = yMin + (zoomBox.Top * ((yMax - yMin) / picCanvas.ClientSize.Height));
+
+                xMax = xMin + (zoomBox.Right * ((xMax - xMin) / picCanvas.ClientSize.Width));
+                yMax = yMin + (zoomBox.Bottom * ((yMax - yMin) / picCanvas.ClientSize.Height));
+                xMin = newXMin;
+                yMin = newYMin;
+
+                DrawMandelbrot(xMin, xMax, yMin, yMax);
+            }
+        }
+
+        // Clears and resets the picturebox
+        private void BttnReset_Click(object sender, EventArgs e)
+        {
+            xMin = -2;
+            xMax = 2;
+            yMin = -2;
+            yMax = 2;
+
+            picCanvas.Image = null;
+        }
+    }
+}
